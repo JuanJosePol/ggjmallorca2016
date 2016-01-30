cfed6c94e163884bde1c54b7d0756cf803a0fef2
Juan Jose Pol <juanjosepol@gmail.com>
2016-01-30 12:41:26 +0100

    Added CheckInZone and WaitingQueue. First version with staff processing.

--- Assets/Scripts/Staff.cs
+++ Assets/Scripts/Staff.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Staff : MonoBehaviour
 {
+    [HideInInspector]
+    public Walker walker;
 
     void Awake()
     {
         GameManager.instance.staff.Add(this);
+        walker = gameObject.AddComponent<Walker>();
     }
 
     public void OnClick()
--- Assets/Scripts/WaitingQueue.cs
+++ Assets/Scripts/WaitingQueue.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class WaitingQueue : MonoBehaviour
 {
@@ -8,13 +9,15 @@ public class WaitingQueue : MonoBehaviour
     [HideInInspector]
     public Transform[] positions;
 
+    private List<Jammer> jammers = new List<Jammer>();
+
     void Awake()
     {
         positions = new Transform[MaxCapacity];
 
         for (int i = 0; i < MaxCapacity; i++)
         {
-            var t = new GameObject().transform;
+            var t = new GameObject("wait"+i).transform;
             t.parent = this.transform;
             t.localPosition = Vector3.zero - Vector3.forward * i * interspace;
             positions[i] = t;
@@ -23,20 +26,38 @@ public class WaitingQueue : MonoBehaviour
 
     public void AddJammer(Jammer jammer)
     {
-
+        jammers.Add(jammer);
+        jammer.walker.MoveTo(positions[jammers.Count - 1].position);
     }
 
-    public void GetNextJammer(Jammer jammer)
+    public Jammer GetNextJammer()
     {
 
+        Jammer j = jammers[0];
+        jammers.RemoveAt(0);
+        for (int i = 0; i < jammers.Count; i++)
+        {
+            jammers[i].walker.MoveTo(positions[i].position);
+        }
+        return j;
+    }
+
+    public bool isFull
+    {
+        get { return jammers.Count >= MaxCapacity; }
+    }
+
+    public bool isEmpty
+    {
+        get { return jammers.Count == 0; }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        foreach (Transform t in positions)
+        for (int i = 0; i < MaxCapacity; i++)
         {
-            Gizmos.DrawSphere(t.position, 0.5f);
+            Gizmos.DrawSphere(transform.position - transform.forward * i * interspace, 0.5f);
         }
     }
 }
