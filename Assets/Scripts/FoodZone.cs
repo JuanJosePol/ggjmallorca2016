d7e8581db7d827bc5bb2fe5f4534f62503e79dc0
Juan Jose Pol <juanjosepol@gmail.com>
2016-01-30 23:17:30 +0100

    Fixed character shaking on tables. Workplaces are claimed on demand.

--- Assets/Scripts/FoodZone.cs
+++ Assets/Scripts/FoodZone.cs
@@ -3,9 +3,41 @@ using System.Collections;
 
 public class FoodZone : MonoBehaviour
 {
+    public float eatTime = 3;
+    public Transform eatingPosition;
+    public WaitingQueue waitingQueue;
+    public MeshFilter pizzas;
     public MeshFilter[] foodAnimation;
 
-    public void AddJammer()
+    [HideInInspector]
+    public Jammer jammer;
+
+    public bool CanEnterJammer()
+    {
+        return !waitingQueue.isFull;
+    }
+
+    public void AddJammer(Jammer newJammer)
+    {
+        if (!CanEnterJammer()) throw new System.Exception("No more jammers allowed in FoodZone");
+
+        if (this.jammer == null)
+        {
+            this.jammer = newJammer;
+            jammer.walker.MoveTo(eatingPosition.position, true, OnJammerReady);
+        }
+        else
+        {
+            waitingQueue.AddJammer(newJammer);
+        }
+    }
+
+    private void OnJammerReady()
+    {
+        Process();
+    }
+
+    public void Process()
     {
 
     }
