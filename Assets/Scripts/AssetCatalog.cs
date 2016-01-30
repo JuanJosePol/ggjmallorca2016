e2990bf2e92061ba95265ee08812acc6397428a2
Javi Cepa <javicepa@gmail.com>
2016-01-30 12:50:05 +0100

    Added character generator

---
+++ Assets/Scripts/AssetCatalog.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetCatalog : MonoBehaviour {
+	
+	public static AssetCatalog instance;
+	
+	public Mesh[] bodies;
+	public Mesh[] eyes;
+	public Mesh[] hairs;
+	public Mesh[] hats;
+	public Mesh[] heads;
+	
+	void Awake() {
+		instance=this;
+		bodies	= Resources.LoadAll<Mesh>("VoxelModels/Bodies");
+		eyes	= Resources.LoadAll<Mesh>("VoxelModels/Eyes");
+		hairs	= Resources.LoadAll<Mesh>("VoxelModels/Hairs");
+		hats	= Resources.LoadAll<Mesh>("VoxelModels/Hats");
+		heads	= Resources.LoadAll<Mesh>("VoxelModels/Heads");
+	}
+	
+	void Update () {
+	
+	}
+}
+
+public static class CatalogExtensions {
+	public static T GetRandom<T>(this T[] array) {
+		return array[Random.Range(0, array.Length-1)];
+	}
+}
---
+++ Assets/Scripts/CharacterGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterGenerator : MonoBehaviour {
+	
+	public GameObject headParent;
+	public GameObject bodyParent;
+	
+	public MeshFilter faceBase;
+	public MeshFilter hatBase;
+	public MeshFilter hairBase;
+	public MeshFilter eyesBase;
+	public MeshFilter bodyBase;
+	
+	public Texture2D skinPalette;
+	public Texture2D hairPalette;
+	
+	void Start () {
+		GenerateChar();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.G)) {
+			GenerateChar();
+		}
+	}
+	
+	void GenerateChar() {
+		faceBase.GetComponent<MeshRenderer>().material.color=GetRandomColorFromPalette(skinPalette);
+		hairBase.GetComponent<MeshRenderer>().material.color=GetRandomColorFromPalette(hairPalette);
+		
+		hairBase.mesh=AssetCatalog.instance.hairs.GetRandom();
+		bodyBase.mesh=AssetCatalog.instance.bodies.GetRandom();
+		hatBase.mesh=AssetCatalog.instance.hats.GetRandom();
+	}
+	
+	Color GetRandomColorFromPalette(Texture2D palette) {
+		int selected=Random.Range(0, palette.width-1);
+		return palette.GetPixel(selected, 0);
+	}
+}
