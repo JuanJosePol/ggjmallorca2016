d6bdce9d5995ca760588fcf2b431bd3cca9fd0c3
Javi Cepa <javicepa@gmail.com>
2016-01-30 18:23:22 +0100

    Added beautiful buttons

---
+++ Assets/Scripts/InfoSlotController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InfoSlotController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
