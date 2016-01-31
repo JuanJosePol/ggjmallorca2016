3209067239f66d4ca364a02e1be2fab1921dfa8a
Juan Jose Pol <juanjosepol@gmail.com>
2016-01-31 04:57:27 +0100

    Added navigation with mouse.

---
+++ Assets/Scripts/NavMeshNavigator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshNavigator : MonoBehaviour
+{
+    void Update()
+    {
+        if (GameManager.instance.selectedStaff == null)
+            return;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.layer != LayerMask.NameToLayer("Clickable"))
+                {
+                    NavMeshHit navHit;
+                    if (NavMesh.SamplePosition(hitInfo.point, out navHit, 0.1f, NavMesh.AllAreas))
+                    {
+                        GameManager.instance.selectedStaff.walker.MoveTo(navHit.position, false);
+                    }
+
+                }
+            }
+        }
+    }
+}
